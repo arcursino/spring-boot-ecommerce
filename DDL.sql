d890202a1fed575f38922c68fbd96ebc02d1361a
arcursino <arcursino@gmail.com>
2021-04-11 16:40:14 +0000

    arrum ddl

--- DDL.sql
+++ DDL.sql
@@ -6,6 +6,7 @@ create user 'user'@'localhost' identified by 'pass123';
 
 grant select, insert, delete, update on ecomerce.* to user@'localhost';
 
+-- Cria tabela cliente
 create table cli_cliente (
   cli_id bigint unsigned not null auto_increment,
   cli_nome varchar(20) not null,
@@ -16,27 +17,25 @@ create table cli_cliente (
   UNIQUE KEY uni_cli_email (cli_email)  
 );
 
+-- Cria tabela Pedido
 create table ped_pedido (
   ped_id bigint unsigned not null auto_increment,
   ped_nome varchar(50) not null,
   ped_valor int not null,
   cli_id bigint unsigned not null,
   primary key (ped_id),  
-  unique key uni_ped_nome (ped_nome)
+  unique key uni_ped_nome (ped_nome),
   foreign key pedcli_cli_fk (cli_id) references cli_cliente (cli_id) on delete restrict on update cascade
 );
 
-
-
 insert into cli_cliente(cli_nome, cli_email, cli_idade)
-    values('Ariana', 'ariana@ariana.com', 37);
+    values('Ariana', 'ariana@email.com', 37);
 insert into cli_cliente(cli_nome, cli_email, cli_idade)
-    values('Ariana1', 'ariana1@ariana.com', 37);
+    values('Ariana1', 'ariana1@email.com', 37);
+
 
 
 insert into ped_pedido(ped_nome, ped_valor, cli_id)
     values('pedido01', 200, 1);
 insert into ped_pedido(ped_nome, ped_valor, cli_id)
     values('pedido02', 400, 2);
-
-
